6ce2b63780f7d38291febd2a074d433c3bed3d41
abdirahman-abass-diss <abdirahman.diss@student.moringaschool.com>
2023-12-20 19:53:08 +0300

    finished

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"{self.name} says: Hello World!")
+
+    def walk(self):
+        print(f"{self.name} is walking.")
+
+# Example usage:
+john = Person("John")
+print(john.name)   # Output: John
+john.talk()        # Output: John says: Hello World!
+john.walk()        # Output: John is walking.
